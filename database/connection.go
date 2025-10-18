1717e03ce6b72539c5bff3a2af72e9f29b8421e8
Wiku Galindra Wardhana <67788111+wikugw@users.noreply.github.com>
2025-10-18 19:28:06 +0700

    update ssl mode connect db

--- database/connection.go
+++ database/connection.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func Connect() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
